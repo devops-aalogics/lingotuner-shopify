ac97a6ac4676ceca2f1e5a29c6547e8b2261cdc7
Wajahat Khan <wajahat.aalogics@gmail.com>
2026-04-27 11:41:12 +0500

    fix: use named exports for react-router handler

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
-import server from "../build/server/index.js";
-export default server;
+import * as build from "../build/server/index.js";
+import { createRequestHandler } from "@react-router/express";
+import express from "express";
+
+const app = express();
+
+app.use(
+  createRequestHandler({
+    build,
+    mode: process.env.NODE_ENV,
+  })
+);
+
+export default app;
